8d355eac94d5b9158ec97fe05f14bc5bafb9ecca
Abdullah Ahmed <abdullahahmed@Abdullahs-MacBook-Pro.local>
2025-03-25 15:27:48 +0500

    CORS updation

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,13 +31,13 @@ origins = allowed_origins.split(",")
 
 logger.info(f"CORS allowed origins: {origins}")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['https://frontend.d2u29wywsqwu0k.amplifyapp.com/'],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+#app.add_middleware(
+#    CORSMiddleware,
+#    allow_origins=["https://frontend.d2u29wywsqwu0k.amplifyapp.com"],
+#    allow_credentials=True,
+#    allow_methods=["*"],
+#    allow_headers=["*"],
+#)
 
 # Include routers
 app.include_router(auth.router)
